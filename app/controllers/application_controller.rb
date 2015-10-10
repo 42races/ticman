464e2db537a923590f7e57aed878d26bbe05ca43
Deepak Kumar <deepakkumarnd@gmail.com>
2015-10-10 18:25:49 +0530

    random string generation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def random_string
+    [*'a'..'z', *'A'..'Z', *'0'..'9'].shuffle.take(8).join
+  end
 end
