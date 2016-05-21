32eed0a18eacd9d76882427ea8fe45caee8dec4c
Deepak Kumar <deepakkumarnd@gmail.com>
2016-05-21 22:35:54 +0530

    add fileds to events table

--- app/models/tic.rb
+++ app/models/tic.rb
@@ -1,5 +1,12 @@
 class Tic
   include Mongoid::Document
+  include Mongoid::Timestamps::Created
 
   paginates_per 50
+
+  field :event, type: String
+  field :trace_id, type: Integer
+
+  index({ event: 1 })
+  index({ created_at: 1 })
 end
